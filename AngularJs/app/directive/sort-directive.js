f251a09ebd5688ee1383064178358320c1b9080f
Manuel Jimenez <manuel.jimenez.dev@gmail.com>
2015-08-11 14:21:17 -0400

    Missing sort directive

---
+++ AngularJs/app/directive/sort-directive.js
@@ -0,0 +1,40 @@
+﻿angular.module('myApp.directive.tablesorting', [])
+
+  .directive("sort", function () {
+
+    return {
+
+        restrict: 'A',
+
+        transclude: true,
+
+        template: '<a ng-click="onClick()">' +
+                  '<span ng-transclude></span>' +
+                  '<i class="glyphicon" ng-class="{\'glyphicon-sort-by-alphabet\' : order === by && !reverse,  \'glyphicon-sort-by-alphabet-alt\' : order===by && reverse}"></i>' +
+                  '</a>',
+
+        scope: {
+            order: '=',
+            by: '=',
+            reverse: '='
+        },
+
+        link: function (scope, element, attrs) {
+
+            scope.onClick = function () {
+
+                if (scope.order === scope.by) {
+                    scope.reverse = !scope.reverse
+                } else {
+                    scope.by = scope.order;
+                    scope.reverse = false;
+
+                }
+
+            }
+
+        }
+
+    }
+
+});
